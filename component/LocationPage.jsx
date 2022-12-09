2afbc48aa2ccd9fa0a76c79242f169d1925d8069
Haorsis <cadetgauthier3@gmail.com>
2022-12-09 16:32:46 +0100

    text for location

--- component/LocationPage.jsx
+++ component/LocationPage.jsx
@@ -1,15 +1,13 @@
 import { View, Text, Pressable, Image } from 'react-native';
 import { useEffect, useState } from 'react';
-import { ThemeContext, themes as styles } from '../theme/ThemeContext';
+import { themes as styles } from '../theme/ThemeContext';
 import { LoadingSpinner } from "./LoadingSpinner";
 import { FlyDistanceCalcul } from '../service/FlyDistanceCalcul';
 import useLanguage from '../utils/useLanguage';
-import * as RNLocalize from 'react-native-localize';
 import useGeolocation from '../utils/useGeolocation';
 
 export function LocationPage({ navigation }) {
-
-  const { selectedLanguage } = useLanguage(RNLocalize.getCountry().toLowerCase());
+  const selectedLanguage = useLanguage();
   const [distance, setDistance] = useState();
   const { location } = useGeolocation();
   const santaImage = require('./images/santa_white.png');
@@ -31,11 +29,11 @@ export function LocationPage({ navigation }) {
           <View style={styles.imgContainer}>
             <Image style={styles.spruceImg} source={santaImage} />
           </View>
-          {location ? <Text style={[styles.whiteText, styles.santaText]}>Santa is at {distance} kms from you </Text> : <LoadingSpinner ></LoadingSpinner>}
+          {location ? <Text style={[styles.whiteText, styles.santaText]}>{selectedLanguage.distanceSanta.replace("{0}", distance)}</Text> : <LoadingSpinner ></LoadingSpinner>}
           <Pressable
             style={[styles.greenButton, styles.alignCenter]}
             onPress={() => navigation.navigate('Home')}>
-            <Text style={styles.buttonText}>{selectedLanguage.words.backButton}</Text>
+            <Text style={styles.buttonText}>{selectedLanguage.backButton}</Text>
           </Pressable>
         </View>
       </View>
