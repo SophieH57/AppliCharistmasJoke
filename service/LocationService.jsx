595b7b8bafbca9a51eeeb87edb8296abf8586979
herb_sophie <marin@LAPTOP-CT2L5BNK>
2022-12-09 11:28:27 +0100

    stage modif

--- service/LocationService.jsx
+++ service/LocationService.jsx
@@ -1,34 +1,36 @@
 import Geolocation from 'react-native-geolocation-service';
 
-export async function locationService(setLocation) {
+export function locationService(setLocation) {
 
     let result;
     // Function to get permission for location
-    try {
-        const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-            {
-                title: 'Geolocation Permission',
-                message: 'Can we access your location?',
-                buttonNeutral: 'Ask Me Later',
-                buttonNegative: 'Cancel',
-                buttonPositive: 'OK',
-            },
-        );
-        console.log('granted', granted);
-        if (granted === 'granted') {
-            console.log('You can use Geolocation');
-            result = true;
-        } else {
-            console.log('You cannot use Geolocation');
-            result = false;
+    async function requestLocationPermission() {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    title: 'Geolocation Permission',
+                    message: 'Can we access your location?',
+                    buttonNeutral: 'Ask Me Later',
+                    buttonNegative: 'Cancel',
+                    buttonPositive: 'OK',
+                },
+            );
+            console.log('granted', granted);
+            if (granted === 'granted') {
+                console.log('You can use Geolocation');
+                return true;
+            } else {
+                console.log('You cannot use Geolocation');
+                return false;
+            }
+        } catch (err) {
+            return false;
         }
-    } catch (err) {
-        result = false;
     }
 
-    console.log(result);
-
+    result = requestLocationPermission();
+    console.log('result : ', result);
     result.then(res => {
         console.log('res is:', res);
         if (res) {
