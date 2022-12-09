20d94bbc59738f538729ee195c6156ced49249ca
Haorsis <cadetgauthier3@gmail.com>
2022-12-09 16:41:15 +0100

    format properly

--- component/LocationPage.jsx
+++ component/LocationPage.jsx
@@ -1,25 +1,32 @@
-import { View, Text, Pressable, Image } from 'react-native';
-import { useEffect, useState } from 'react';
-import { themes as styles } from '../theme/ThemeContext';
-import { LoadingSpinner } from "./LoadingSpinner";
-import { FlyDistanceCalcul } from '../service/FlyDistanceCalcul';
+import {View, Text, Pressable, Image} from 'react-native';
+import {useEffect, useState} from 'react';
+import {themes as styles} from '../theme/ThemeContext';
+import {LoadingSpinner} from './LoadingSpinner';
+import {FlyDistanceCalcul} from '../service/FlyDistanceCalcul';
 import useLanguage from '../utils/useLanguage';
 import useGeolocation from '../utils/useGeolocation';
 
-export function LocationPage({ navigation }) {
+export function LocationPage({navigation}) {
   const selectedLanguage = useLanguage();
   const [distance, setDistance] = useState();
-  const { location } = useGeolocation();
+  const {location} = useGeolocation();
   const santaImage = require('./images/santa_white.png');
 
   const coordonneesLaponie = {
     latitude: 67.9222,
-    longitude: 26.5046
-  }
+    longitude: 26.5046,
+  };
 
   useEffect(() => {
     if (location)
-      setDistance(FlyDistanceCalcul(location.coords.latitude, location.coords.longitude, coordonneesLaponie.latitude, coordonneesLaponie.longitude).toFixed(1))
+      setDistance(
+        FlyDistanceCalcul(
+          location.coords.latitude,
+          location.coords.longitude,
+          coordonneesLaponie.latitude,
+          coordonneesLaponie.longitude,
+        ).toFixed(1),
+      );
   }, [location]);
 
   return (
@@ -29,7 +36,15 @@ export function LocationPage({ navigation }) {
           <View style={styles.imgContainer}>
             <Image style={styles.spruceImg} source={santaImage} />
           </View>
-          {location ? <Text style={[styles.whiteText, styles.santaText]}>{selectedLanguage.distanceSanta.replace("{0}", distance)}</Text> : <LoadingSpinner ></LoadingSpinner>}
+          <View>
+            {location ? (
+              <Text style={[styles.whiteText, styles.santaText]}>
+                {selectedLanguage.distanceSanta.replace('{0}', distance)}
+              </Text>
+            ) : (
+              <LoadingSpinner></LoadingSpinner>
+            )}
+          </View>
           <Pressable
             style={[styles.greenButton, styles.alignCenter]}
             onPress={() => navigation.navigate('Home')}>
@@ -38,5 +53,5 @@ export function LocationPage({ navigation }) {
         </View>
       </View>
     </>
-  )
+  );
 }
